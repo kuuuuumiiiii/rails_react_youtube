b50dc42b2885d07642ab4693e2e1ca4ca186674f
kuuuuumiiiii <kuuuuumiiiii@users.noreply.github.com>
2024-11-29 23:40:13 +0900

    TodoList作成

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import styled from 'styled-components';
+import 'react-toastify/dist/ReactToastify.css';
+import AddTodo from './components/AddTodo'
+import TodoList from './components/TodoList'
+import EditTodo from './components/EditTodo'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Nabvar = styled.nav`
+  background: #dbfffe;
+  min-height: 8vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`
 
+const Logo = styled.div`
+  font-weight: bold;
+  font-size: 23px;
+  letter-spacing: 3px;
+`
+
+const NavItems = styled.ul`
+  display: flex;
+  width: 400px;
+  max-width: 40%;
+  justify-content: space-around;
+  list-style: none;
+`
+
+const NavItem = styled.li`
+  font-size: 19px;
+  font-weight: bold;
+  opacity: 0.7;
+  &:hover {
+    opacity: 1;
+  }
+`
+
+const Wrapper = styled.div`
+  width: 700px;
+  max-width: 85%;
+  margin: 20px auto;
+`
+
+
+
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Nabvar>
+        <Logo>
+          TODO
+        </Logo>
+        <NavItems>
+          <NavItem>
+            <Link to="/todos">
+              Todos
+            </Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/todos/new">
+              Add New Todo
+            </Link>
+          </NavItem>
+        </NavItems>
+      </Nabvar>
+      <Wrapper>
+        <Routes>
+          <Route path="/" element={<Navigate to="/todos" />} />
+          <Route path="/todos" element={<TodoList />} />
+          <Route path="/todos/new" element={<AddTodo />} />
+          <Route path="/todos/:id/edit" element={<EditTodo />} />
+        </Routes>
+      </Wrapper>
+      <ToastContainer position="bottom-center" hideProgressBar />
+    </Router>
   )
 }
 
